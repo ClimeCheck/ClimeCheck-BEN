5741a153ff39c3535fba296a6125de83b12ae0ef
madeyinka <mutairadeyinka@gmail.com>
2022-12-23 09:03:44 +0100

    Serverless Config

--- api.js
+++ api.js
@@ -1,3 +1,4 @@
+const serverless = require('serverless-http')
 const express = require('express')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv').config()
@@ -18,6 +19,8 @@ app.use(cookieParser())
 
 app.use(require('./controllers'))
 
-app.listen(process.env.PORT, () => {
-    Logger.init({msg:'Listening on http://[:]'+process.env.PORT+_config.app_name+_config.app_base+_config.app_version})
-})
+// app.listen(process.env.PORT, () => {
+//     Logger.init({msg:'Listening on http://[:]'+process.env.PORT+_config.app_name+_config.app_base+_config.app_version})
+// })
+
+module.exports.handler = serverless(app)
